300a92cc23b2d7d01906c677696f885d36345e91
Rebecca Vickers <rvickers153@gmail.com>
2015-04-06 20:07:02 -0400

    Pins default scope to order by most recent first

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600x600>"}
 
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+	default_scope -> { order("created_at DESC") }
 end
